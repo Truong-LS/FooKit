898081320bb0c9039ef3eec87682eb7dca4f5c60
Truong <satruong0@gmail.com>
2026-05-28 12:13:54 +0700

    feat: add database migration to seed default admin and user roles

---
+++ src/FooKit.Infrastructure/Migrations/20260528050830_SeedDefaultUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FooKit.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDefaultUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "Email", "FullName", "IsActive", "PasswordHash", "RoleId", "Username" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000099"), new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), null, "Default User", true, "$2a$11$bjBTzVjWIFb.5L7NGW5IruqxBiquPLihDJV05EQ1zKJezfF5XZzdC", new Guid("00000000-0000-0000-0000-000000000002"), "user" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000099"));
+        }
+    }
+}
